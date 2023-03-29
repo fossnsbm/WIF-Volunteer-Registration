48754ecae1a05858855531d6409564d35b5a8f61
WasathTheekshana <wasath.vt@gmail.com>
2023-03-30 01:40:08 +0530

    feat: Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,17 @@ function Home() {
   return (
     <seciton>
       <div className="pl-10 pt-10 lg:flex lg:flex:row">
-        <div className="flex flex-col gap-9 md:gap-11 pr-10 lg:w-[95%] lg:gap-[7rem] lg:pr-0 lg:mr-0 ">
-          <div className="flex flex-col items-center gap-7 md:gap-10 lg:items-start lg:w-[100%]">
-            <h1 className="font-bold text-6xl md:text-7xl text-center lg:text-left lg:text-8xl">
+        
+        <div className="flex flex-col justify-center gap-20 md:gap-11 pr-10 lg:w-[95%] lg:gap-[6rem] lg:pr-0 lg:mr-0 ">
+          
+          <div className="flex flex-col items-center md:items-start gap-7 md:gap-10 lg:items-start lg:w-[100%]">
+            <h1 className="font-bold text-7xl md:text-8xl text-left lg:text-left lg:text-[7rem]">
               {" "}
-              <span className="text-[#26B2B7]">Girls,</span> It&apos;s your
+              <span className="text-[#26B2B7]">Girls,</span> It&apos;s <br /> your
               chance.
             </h1>
             <div className="md:w-[75%]">
-              <p className="text-center md:text-xl lg:text-left">
+              <p className="text-left md:text-xl lg:text-left">
                 Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et
                 massa mi. Aliquam in hendrerit urna. Pellentesque sit amet
                 sapien.Lorem ipsum dolor sit amet consectetur adipiscing elit Ut
@@ -22,8 +24,11 @@ function Home() {
                 sapien.
               </p>
             </div>
-            <button className="btn-primary mt-5">Become a Volunteer</button>
+            <div>
+              <button className="btn-primary mt-5">Become a Volunteer</button>
+            </div>
           </div>
+
           <div className="flex flex-col items-center lg:items-start">
             <div className="flex gap-4">
               <img src="/FOSSlogo.png" alt="FOSS logo" />
@@ -34,12 +39,14 @@ function Home() {
               />
             </div>
           </div>
+          
         </div>
-        <div className="flex justify-end lg:w-[100%]">
+
+        <div className="flex justify-end items-end lg:w-[100%]">
           <img
             src="/heroImg.png"
             alt="WIF Hero"
-            className="right-0 w-[80%] md:w-[65%] lg:w-auto"
+            className="right-0 w-[80%] md:w-[65%] lg:w-[100%]"
           />
         </div>
       </div>

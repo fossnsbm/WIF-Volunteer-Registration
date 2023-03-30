12f8a10c36a0c8895235ce58df33cd9eef3e5e0e
WasathTheekshana <wasath.vt@gmail.com>
2023-03-30 10:00:09 +0530

    fix: Text sizes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,13 +8,13 @@ function Home() {
         <div className="flex flex-col justify-center gap-20 md:gap-11 pr-10 lg:w-[95%] lg:gap-[6rem] lg:pr-0 lg:mr-0 ">
           
           <div className="flex flex-col items-center md:items-start gap-7 md:gap-10 lg:items-start lg:w-[100%]">
-            <h1 className="font-bold text-7xl md:text-8xl text-left lg:text-left lg:text-[7rem]">
+            <h1 className="font-bold text-7xl md:text-8xl text-center lg:text-left lg:text-[7rem]">
               {" "}
               <span className="text-[#26B2B7]">Girls,</span> It&apos;s <br /> your
               chance.
             </h1>
             <div className="md:w-[75%]">
-              <p className="text-left md:text-xl lg:text-left">
+              <p className="text-center md:text-xl lg:text-left">
                 Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et
                 massa mi. Aliquam in hendrerit urna. Pellentesque sit amet
                 sapien.Lorem ipsum dolor sit amet consectetur adipiscing elit Ut
